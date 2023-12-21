4bc28cfa20f91d477ca1a93321e057838dd2268f
Ali Ahmed Shaikh <33371550+AliAhmedShaikh@users.noreply.github.com>
2023-12-21 12:06:48 +0500

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/data engineer portfolio.png";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Markowitz portfolio Optimization on S&P 500",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Built a program to optimize the portfolio for gain vs volatility of the stock market. To have the maximum gain increase for the volatility of the portfolio based on the previous 30 years data.",
+    url: "https://github.com/AliAhmedShaikh/Porfolio-Optimization-using-Markowitz-Optimization",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Chatbot Uttarakhand",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Built a Custom built chatbot for classification on a news Uttarkand State news",
+    url: "https://github.com/AliAhmedShaikh/Chatbot_Uttarakhand",
   },
   {
-    title: "My Resume Site",
+    title: "Accidental Data Analysis",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Built an Data Analysis App that helps to visulize the most Acciedents Spots all over the city.",
+    url: "https://github.com/AliAhmedShaikh/streamlit-app",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Super Store Data Anaysis",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Explored SuperStore Data for Spark Internship",
+    url: "https://github.com/AliAhmedShaikh/GRIPAPRIL21/blob/main/Exploratory%20Data%20Analysis-Retail.ipynb",
   },
 ];
 
